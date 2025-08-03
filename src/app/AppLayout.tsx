3b8e80a3d4e7319128fdedd1f83e9a162bef4d56
m1a2023 <mag2017go@gmail.com>
2025-08-03 06:15:00 -0700

    AppTheme fixed;

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -1,15 +1,11 @@
 import { Outlet } from "react-router";
 import Navbar from "../widgets/navbar/Navbar";
-import { ColorTheme } from "../features/theme/ColorTheme";
 
 const AppLayout = (): React.ReactElement => {
 	return (
-		<div
-			data-bs-theme={ColorTheme(true)}
-			className="container-fluid my-3 d-flex flex-column gap-3"
-		>
+		<div className="container-fluid my-3 d-flex flex-column gap-3">
 			<Navbar />
-			<main className="container" data-bs-theme={ColorTheme(true)}>
+			<main className="container">
 				<Outlet />
 			</main>
 		</div>
